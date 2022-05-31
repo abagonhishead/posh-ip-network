81696e2abd8f641df69de8e587c396f9107be42e
Russell Webster <russell@jossell.com>
2022-05-31 18:59:48 +0100

    Shared unit tests

---
+++ Tests/Shared/Jossellware.Shared.Tests/Threading/CancellationTokenSourceFactoryTests.cs
@@ -0,0 +1,38 @@
+﻿namespace Jossellware.Shared.Tests.Threading
+{
+    using FluentAssertions;
+    using Jossellware.Shared.Threading;
+    using Xunit;
+
+    public class CancellationTokenSourceFactoryTests
+    {
+        private readonly ICancellationTokenSourceFactory sut;
+
+        public CancellationTokenSourceFactoryTests()
+        {
+            this.sut = new CancellationTokenSourceFactory();
+        }
+
+        [Fact]
+        public void BuildSource_NoParameters_ReturnsSourceWithNoTimeout()
+        {
+            // Act
+            var result = this.sut.BuildSource();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.IsCancellationRequested.Should().BeFalse();
+        }
+
+        [Fact]
+        public void BuildSource_TimeSpan_ReturnsSourceWithTimeout()
+        {
+            // Act
+            var result = this.sut.BuildSource(TimeSpan.FromSeconds(0));
+
+            // Assert
+            result.Should().NotBeNull();
+            result.IsCancellationRequested.Should().BeTrue();
+        }
+    }
+}
